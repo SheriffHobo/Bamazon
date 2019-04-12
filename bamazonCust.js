e681c0f1b5fb777e718e59caf65abd7973f360c4
SheriffHobo <zombiemaster@gmail.com>
2019-04-12 13:30:59 -0700

    added order check before purchase

--- bamazonCust.js
+++ bamazonCust.js
@@ -42,35 +42,15 @@ var purchaseItem = function () {
 		type: "input",
 		message: "How many would you like to purchase?",
 
-		// Pings DB for item details
+	// Pings DB for item details
 	}]).then(function (answer) {
 		connection.query('SELECT item, catagory, price, stock FROM products WHERE ?', {
 			id: answer.id
 		}, function (err, res) {
 			console.log('\nYou\'ve selected ' + answer.quantity + ' ' + res[0].item + ' at $' + res[0].price + ' each');
 
-			// purchasePrompt(); or something to present more questions. Refer to line 98
-			// If YES, continue with purchase flow
+			purchasePrompt(res, answer.quantity, answer.id);
 
-			if (res[0].stock >= answer.quantity) {
-				//If enough inventory to complete order, process order by updating database inventory and notifying customer that order is complete. 
-				var itemQuantity = res[0].stock - answer.quantity;
-				connection.query("UPDATE products SET ? WHERE ?", [{
-						stock: itemQuantity
-					},
-					{
-						id: answer.id
-					}
-				], function (err, res) {});
-				var cost = res[0].price * answer.quantity;
-				// Order complete
-				console.log('\nOrder complete. Your total is $' + cost.toFixed(2) + '\n');
-				customerPrompt();
-			} else {
-				// Order incomplete
-				console.log('\nSorry, not enough inventory for fulfill your order.\n');
-				customerPrompt();
-			};
 		});
 	});
 };
@@ -95,22 +75,43 @@ var customerPrompt = function () {
 	});
 };
 
-var purchasePrompt = function () {
+// Asks user if the order is correct, processes if yes, takes user back to selection if no.
+var purchasePrompt = function (res, quantity, id) {
 	inquirer.prompt({
 		name: "action",
 		type: "list",
 		message: "Is this the correct order?\n",
 		choices: ["Yes", "No"]
 	}).then(function (answer) {
+		let navTo = customerPrompt;
 		switch (answer.action) {
 			case 'Yes':
-				// Continue with purchase flow, line 55
+				if (res[0].stock >= parseInt(quantity)) {
+					//If enough inventory to complete order, process order by updating database inventory and notifying customer that order is complete. 
+					var itemQuantity = res[0].stock - parseInt(quantity);
+					connection.query("UPDATE products SET ? WHERE ?", [{
+							stock: itemQuantity
+						},
+						{
+							id: id
+						}
+					], function (err, res) {});
+					var cost = res[0].price * parseInt(quantity);
+					// Order complete
+					console.log('\nOrder complete. Your total is $' + cost.toFixed(2) + '\n');
+					// customerPrompt();
+				} else {
+					// Order incomplete
+					console.log('\nSorry, not enough inventory for fulfill your order.\n');
+					// customerPrompt();
+				};
 				break;
 
 			case 'No':
-				purchaseItem(); 
+				navTo = purchaseItem; 
 				break;
 		};
+		navTo();
 	});
 };
 
